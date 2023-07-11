b7bc328c973f3f58b2bdb2a15561c0de805d8e11
John Alarcon <j.alarcon@outlook.com>
2023-07-11 14:09:24 -0400

    rearranged required paths

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const app = express();
+const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const path = require('path');
-const PORT = process.env.PORT || 3001;
+const express = require('express');
+const app = express();
+
+
 const allNotes = require('./db/db.json');
 
 app.use(express.urlencoded({extended: true}));

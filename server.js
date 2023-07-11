582c34edc61acfb7b6b1a427279aed87b16e0856
John Alarcon <j.alarcon@outlook.com>
2023-07-11 14:30:03 -0400

    more typos

--- server.js
+++ server.js
@@ -16,15 +16,15 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(_dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(_dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(_dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 function createNewNote(body, notesArray) {
